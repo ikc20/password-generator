8978a4751ccaf14256ea285c3b4014a7640cbd47
ikram chahdi <120470168+ikc20@users.noreply.github.com>
2025-10-08 12:06:41 +0100

    Update build.zig and improve .gitignore

--- build.zig
+++ build.zig
@@ -1,13 +1,25 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const lib = b.addSharedLibrary(.{
-        .name = "passwordgen",
-        .root_source_file = .{ .path = "src/password_lib.zig" },
-        .version = .{ .major = 1, .minor = 0, .patch = 0 },
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
-    });
+    // Bibliothèque partagée
+    const lib = b.addSharedLibrary("passwordgen", null);
+    lib.addSourceFile("src/password_lib.zig");
+    lib.setOutputDir("lib");
+    lib.setBuildMode(.ReleaseSafe);
     
-    b.installArtifact(lib);
+    // Exécutable JSON
+    const exe_json = b.addExecutable("main_json", null);
+    exe_json.addSourceFile("src/main_json.zig");
+    exe_json.setOutputDir("bin");
+    exe_json.setBuildMode(.ReleaseSafe);
+    
+    // Exécutable principal
+    const exe_main = b.addExecutable("password-generator", null);
+    exe_main.addSourceFile("src/main.zig");
+    exe_main.setOutputDir("bin");
+    exe_main.setBuildMode(.ReleaseSafe);
+    
+    b.default_step.dependOn(&lib.step);
+    b.default_step.dependOn(&exe_json.step);
+    b.default_step.dependOn(&exe_main.step);
 }
